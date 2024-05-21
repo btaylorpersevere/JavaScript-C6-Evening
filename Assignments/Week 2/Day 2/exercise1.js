9467b57426401400809b67975809d7c0bde403fa
btaylorpersevere <btaylor@perseverenow.org>
2024-05-21 18:24:11 -0500

    updated homework for week 2 day 2

---
+++ Assignments/Week 2/Day 2/exercise1.js
@@ -0,0 +1,10 @@
+// 1. Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript'.
+// 2. Print the string on the browser console using console.log()
+// 3. Print the length of the string on the browser console using console.log()
+// 4. Change all the string characters to capital letters using toUpperCase() method
+// 5. Change all the string characters to lowercase letters using toLowerCase() method
+// 6. Cut (slice) out the first word of the string using substr() or substring() method
+// 7. Slice out the phrase Days Of JavaScript from 30 Days Of JavaScript.
+// 8. Check if the string contains a word Script using includes() method
+// 9. Split the string into an array using split() method
+// 10.Split the string 30 Days Of JavaScript at the space using split() method

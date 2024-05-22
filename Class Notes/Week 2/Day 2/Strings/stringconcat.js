85516a09f272ecc1d5bd2dd78e4730a95610fdca
btaylorpersevere <btaylor@perseverenow.org>
2024-05-22 18:38:09 -0500

    updates

--- Class Notes/Week 2/Day 2/Strings/stringconcat.js
+++ Class Notes/Week 2/Day 2/Strings/stringconcat.js
@@ -1,29 +1,29 @@
-//String Concatenation
-// Connecting two or more strings together is called concatenation. Using the strings declared in the previous String section:
+// //String Concatenation
+// // Connecting two or more strings together is called concatenation. Using the strings declared in the previous String section:
 
-let fullName = firstName + space + lastName; // concatenation, merging two string together.
-console.log(fullName);
+// let fullName = firstName + space + lastName; // concatenation, merging two string together.
+// console.log(fullName);
 
-let fullName = firstName + space + lastName; // concatenation, merging two string together.
-console.log(fullName);
+// let fullName = firstName + space + lastName; // concatenation, merging two string together.
+// console.log(fullName);
 
-// We can concatenate strings in different ways.
+// // We can concatenate strings in different ways.
 
-// Concatenating Using Addition Operator
-// Concatenating using the addition operator is an old way. This way of concatenating is tedious and error-prone. It is good to know how to concatenate this way, but I strongly suggest to use the ES6 template strings (explained later on).
+// // Concatenating Using Addition Operator
+// // Concatenating using the addition operator is an old way. This way of concatenating is tedious and error-prone. It is good to know how to concatenate this way, but I strongly suggest to use the ES6 template strings (explained later on).
 
-// Declaring different variables of different data types
-let space = ' '
-let firstName = 'Asabeneh'
-let lastName = 'Yetayeh'
-let country = 'Finland'
-let city = 'Helsinki'
-let language = 'JavaScript'
-let job = 'teacher'
-let age = 250
+// // Declaring different variables of different data types
+// let space = ' '
+// let firstName = 'Asabeneh'
+// let lastName = 'Yetayeh'
+// let country = 'Finland'
+// let city = 'Helsinki'
+// let language = 'JavaScript'
+// let job = 'teacher'
+// let age = 250
 
-let fullName =firstName + space + lastName
-let personInfoOne = fullName + '. I am ' + age + '. I live in ' + country; // ES5 string addition
+// let fullName =firstName + space + lastName
+// let personInfoOne = fullName + '. I am ' + age + '. I live in ' + country; // ES5 string addition
 
-console.log(personInfoOne)
-// Asabeneh Yetayeh. I am 250. I live in Finland
+// console.log(personInfoOne)
+// // Asabeneh Yetayeh. I am 250. I live in Finland

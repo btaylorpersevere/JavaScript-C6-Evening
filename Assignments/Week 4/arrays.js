78c80fd706d96db7cad55619e1b65fceb6063e7a
btaylorpersevere <btaylor@perseverenow.org>
2024-06-03 20:32:30 -0500

    updated homework

---
+++ Assignments/Week 4/arrays.js
@@ -0,0 +1,34 @@
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+  ]
+  
+  const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB'
+  ]
+  Declare an empty array;
+  Declare an array with more than 5 number of elements
+  Find the length of your array
+  Get the first item, the middle item and the last item of the array
+  Declare an array called mixedDataTypes, put different data types in the array and find the length of the array. The array size should be greater than 5
+  Declare an array variable name itCompanies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon
+  Print the array using console.log()
+  Print the number of companies in the array
+  Print the first company, middle and last company
+  Print out each company
+  Change each company name to uppercase one by one and print them out
